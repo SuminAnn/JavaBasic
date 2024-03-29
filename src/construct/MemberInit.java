40f027069881676f53d549eb92392159c8c293b9
SuminAnn <annsumin2066@naver.com>
2024-03-29 11:25:00 +0900

    this 관련

--- src/construct/MemberInit.java
+++ src/construct/MemberInit.java
@@ -4,6 +4,12 @@ public class MemberInit {
     String name;
     int age;
     int grade;
+
+    void initMember(String name, int age, int grade){
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
 }
 /*
  * 생성자 : 객체를 생성하는 시점에 어떤 작업을 하고싶은 경우에 이용
