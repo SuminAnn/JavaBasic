034517201d194b14fdbd25b95f02abc8ba50be04
SuminAnn <annsumin2066@naver.com>
2024-05-07 17:06:29 +0900

    Polymorphism

--- src/poly/car0/CarMain0.java
+++ src/poly/car0/CarMain0.java
@@ -5,8 +5,17 @@ public class CarMain0 {
     public static void main(String[] args) {
         Driver driver = new Driver();
         K3Car k3Car = new K3Car();
-
         driver.setK3Car(k3Car);
+        driver.drive();
+
+        //추가
+        Model3Car model3Car = new Model3Car();
+        driver.setK3Car(null);
+        driver.setModel3Car(model3Car);
         driver.drive();
     }
 }
+/*
+ * 다형성이 없는 경우의 문제점
+ * 자동차가 늘어날수록 운전자의 코드를 계속 수정 및 추가를 해줘야한다
+ */
---
+++ src/poly/car1/CarMain.java
@@ -0,0 +1,18 @@
+package poly.car1;
+
+public class CarMain {
+
+    public static void main(String[] args) {
+        Driver driver = new Driver();
+
+        //차량 선택(k3)
+        Car k3Car = new K3Car();
+        driver.setCar(k3Car);
+        driver.dirve();
+
+        //차량 변경(k3 -> model3)
+        Car model3Car = new Model3Car();
+        driver.setCar(model3Car);
+        driver.dirve();
+    }
+}
